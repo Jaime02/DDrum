060b29d3a8b7b1311c722dda7e119e968db644b1
Jaime Resano <Jaime.Resano-Aisa@qt.io>
2024-12-05 12:03:58 +0100

    Before trying Knud approach

--- Python/main.py
+++ Python/main.py
@@ -1,24 +1,28 @@
-# Copyright (C) 2024 The Qt Company Ltd.
-# SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
-
 import sys
+from pathlib import Path
 
 from PySide6.QtGui import QGuiApplication
 from PySide6.QtQml import QQmlApplicationEngine
 from PySide6.QtCore import QDirIterator
 
-# The .qrc resource file is compiled by Qt Design Studio to resources.py
-# NOTE: When the resource content changes, the file needs to be recompiled again
-import autogen.resources  # noqa: E402
+from autogen.settings import qml_app_url
 
-# TODO: check how to avoid this imports or generate them in DS
+try:
+    import autogen.resources  # noqa: E402
+except ImportError:
+    print(
+        f"Error: No compiled resources found in "
+        f"{(Path(__file__) / 'autogen' / 'resources.py').absolute()}",
+        f"Please compile the resources using pyside6-rcc or pyside6-project build",
+        file=sys.stderr,
+    )
+    sys.exit(1)
+
 import audioengine  # noqa: E402
 import waveformitem  # noqa: E402
 
 
 def main():
-    qml_app_url = ":/qt/qml/DDrumContent/App.qml"
-
     app = QGuiApplication(sys.argv)
     engine = QQmlApplicationEngine()
 
